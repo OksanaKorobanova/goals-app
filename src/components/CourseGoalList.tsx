2e248953b7355a5645dd2b7bcd5c60a8572e12ba
Oksana Korobanova <58445023+OksanaKorobanova@users.noreply.github.com>
2023-12-04 11:58:59 +0100

    added infobox

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,6 +1,7 @@
-import { type FC } from 'react';
+import { type FC, type ReactNode } from 'react';
 import CourseGoal from './CourseGoal.tsx';
 import { type CourseGoal as CourseGoalType } from '../App.tsx';
+import InfoBox from './InfoBox.tsx';
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
@@ -8,18 +9,37 @@ type CourseGoalListProps = {
 };
 
 const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDeleteGoal }) => {
+  if (goals.length === 0) {
+    return <InfoBox mode='hint'>You have no goals yet. Start adding</InfoBox>;
+  }
+
+  let waningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    waningBox = (
+      <InfoBox mode='warning' severity='high'>
+        Too many goals. Better to stick with 3 goals
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => {
-        return (
-          <li key={goal.id}>
-            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-              <p>{goal.description}</p>
-            </CourseGoal>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {waningBox}
+      <ul>
+        {goals.map((goal) => {
+          return (
+            <li key={goal.id}>
+              <CourseGoal
+                id={goal.id}
+                title={goal.title}
+                onDelete={onDeleteGoal}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
